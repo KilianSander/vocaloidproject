bebb96b553f003884159349c76f231c547185c77
Kilian <kilian.sander@aol.com>
2024-12-15 21:56:00 +0100

    document dictionary

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Dictionary for the Project
+#'
+#' This is the dictionary used for internationalization.
+#' It is an `i18n_dict` class object (see [psychTestR::i18n_dict]).
+#' The dictionary contains German (`de` and `de_f`) and Japanese (`ja`)
+#' as well as English (`en`) translations.
+#'
+#' @references
+#' @docType data
+#' @name vocaloidproject_dict
+#'
+"vocaloidproject_dict"
