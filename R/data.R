bac602360a12c197c254487976b33cfb6c7c2fa1
Kilian Sander <kilian.sander@aol.com>
2024-12-17 13:25:56 +0100

    document the project's dictionary

--- R/data.R
+++ R/data.R
@@ -5,7 +5,37 @@
 #' The dictionary contains German (`de` and `de_f`) and Japanese (`ja`)
 #' as well as English (`en`) translations.
 #'
+#' @details
+#' The dictionary includes the Goldsmiths Musical Sophistication Index
+#' (Gold-MSI) as well as general questionnaire formulations.
+#'
+#' Everything regarding Gold-MSI is taken from Müllensiefen et al. (2014),
+#' Sadakata et al. (2022), and Schaal et al. (2014).
+#' Some general formulations in English and German are taken from `psyquest`
+#' (Höger et al., 2024).
+#'
 #' @references
+#' Höger, F., Frieler, K., Pausch, V., Sander, K., & Ruth, N. (2024).
+#' *psyquest* (Version 1.5.9) \[R package\].
+#' <https://github.com/klausfrieler/psyquest>
+#'
+#' Müllensiefen, D., Gingras, B., Musil, J., & Stewart, L. (2014).
+#' The musicality of non-musicians: An index for assessing musical
+#' sophistication in the general population. *PLoS ONE*, *9*(2), Article e89642.
+#' <https://doi.org/10.1371/journal.pone.0089642>
+#'
+#' Sadakata, M., Yamaguchi, Y., Ohsawa, C., Matsubara, M., Terasawa, H.,
+#' Von Schnehen, A., Müllensiefen, D., & Sekiyama, K. (2022).
+#' The Japanese translation of the Gold-MSI: Adaptation and validation of the
+#' self-report questionnaire of musical sophistication. *Musicae Scientiae*,
+#' *27*(3), 798–810. <https://doi.org/10.1177/10298649221110089>
+#'
+#' Schaal, N. K., Bauer, A.-K. R., & Müllensiefen, D. (2014).
+#' Der Gold-MSI: Replikation und Validierung eines Fragebogeninstrumentes zur
+#' Messung Musikalischer Erfahrenheit anhand einer deutschen Stichprobe.
+#' *Musicae Scientiae*, *18*(4), 423–447.
+#' <https://doi.org/10.1177/1029864914541851>
+#'
 #' @docType data
 #' @name vocaloidproject_dict
 #'
