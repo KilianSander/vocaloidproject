e6031d88fa4ada3fed3236d85331642e3802916b
Kilian <kilian.sander@aol.com>
2025-05-19 15:06:02 +0200

    sketch for voice rating battery

---
+++ R/voice-rating-battery.R
@@ -0,0 +1,60 @@
+voice_rating_battery <- function(title = "Voiceratingbattery",
+                                 admin_password = "vocaloid",
+                                 researcher_email = NULL,
+                                 demo = FALSE,
+                                 dict = vocaloidproject::vocaloidproject_dict,
+                                 allow_any_p_id_url = TRUE,
+                                 force_p_id_from_url = FALSE,
+                                 languages = c("de_f"),
+                                 stimulus_url = character(),
+                                 logo = NULL,
+                                 debug = FALSE) {
+  info_p1 <-
+    shiny::HTML(
+      "Voice rating info"
+    )
+  info_plast <-
+    shiny::HTML(
+      "This is the last page."
+    )
+  elts <-
+    psychTestR::join(
+      psychTestR::one_button_page(
+        body = info_p1,
+        button_text = "Weiter"
+      ),
+      psyquest::DEG(
+        subscales = c("Age", "Gender"),
+        # show_month = FALSE,
+        year_range = c(1925, 2007),
+        dict = dict
+      ),
+      # university student
+      # professional field / field of study
+      first_language_page(
+        dict = dict
+      ),
+      education_page(
+        dict = dict
+      ),
+      psychTestR::final_page(
+        body = info_plast
+      )
+    )
+
+  psychTestR::make_test(
+    elts = elts,
+    opt = psychTestR::test_options(
+      title = title,
+      admin_password = admin_password,
+      researcher_email = researcher_email,
+      demo = demo,
+      languages = languages,
+      allow_any_p_id_url = allow_any_p_id_url,
+      force_p_id_from_url = force_p_id_from_url,
+      logo = logo,
+      logo_width = "300px",
+      logo_height = "auto"
+    )
+  )
+}
