4cc3c90947e3aa2fbf29fd06522c6be7f535eb46
Kilian <kilian.sander@aol.com>
2025-05-20 17:34:18 +0200

    add `student_page()`

--- R/educational-degree.R
+++ R/educational-degree.R
@@ -15,9 +15,35 @@ education_page <- function(label = "educational_degree",
     psychTestR::NAFC_page(
       label = label,
       prompt = psychTestR::i18n("EDU_PROMPT"),
-      choices = as.character(1:6),
+      choices = c("psychology", "music/musician", "musicology",
+                  "sound engineering", "other music-related subjects", "other"),
       labels = purrr::map(paste0("EDU_CHOICE", 1:6), psychTestR::i18n),
       button_style = "width:400px"
+    ),
+    dict = dict,
+    default_lang = default_lang
+  )
+}
+
+#' University student page
+#'
+#' This function creates a page to ask participants whether they are currently
+#' studying at a university.
+#'
+#' @inheritParams first_language_page
+#'
+#' @export
+student_page <- function(label = "student",
+                         dict = vocaloidproject::vocaloidproject_dict,
+                         default_lang = "de_f") {
+  stopifnot(is.scalar.character(label),
+            is.character(default_lang))
+  psychTestR::new_timeline(
+    psychTestR::NAFC_page(
+      label = label,
+      prompt = psychTestR::i18n("STUDENT_PROMPT"),
+      choices = c("student", "non-student"),
+      labels = purrr::map(paste0("STUDENT_CHOICE", 1:2), psychTestR::i18n)
     ),
     dict = dict,
     default_lang = default_lang
