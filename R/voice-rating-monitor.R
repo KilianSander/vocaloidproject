50c0ca1725008177db774cf9de062c7cee1ac717
Kilian <kilian.sander@aol.com>
2025-05-21 23:49:40 +0200

    sketch voice rating monitor app

---
+++ R/voice-rating-monitor.R
@@ -0,0 +1,31 @@
+voice_rating_monitor <- function(battery_folder_name = "voice-rating",
+                                 offline_results_dir = "data-raw") {
+  require(shiny)
+  require(tidyverse)
+  # thanks to Klaus
+  on_server <- grepl("shiny-server", getwd())
+  if (on_server) {
+    results_dir <- file.path(
+      "..", battery_folder_name, "output", "results"
+    )
+  } else {
+    results_dir <- offline_results_dir
+  }
+
+  read_data <- function(results_dir = results_dir) {
+    results <- purrr::map(
+      list.files(path = results_dir, pattern = "*.rds", full.names = TRUE),
+      function(x) {
+        readRDS(x) %>% as.list()
+      }
+    )
+  }
+
+  ui <- fluidPage(
+    #
+  )
+
+  server <- function(input, output, session) {
+    #
+  }
+}
