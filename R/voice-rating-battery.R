a40fdd67db02be83dc76661efd5c7a1f14c29f1c
Kilian <kilian.sander@aol.com>
2025-06-03 16:14:07 +0200

    add info to `voice_rating_battery()`

--- R/voice-rating-battery.R
+++ R/voice-rating-battery.R
@@ -10,7 +10,7 @@
 #' @inheritParams audio_vas_page_battery
 #'
 #' @export
-voice_rating_battery <- function(title = "Voiceratingbattery",
+voice_rating_battery <- function(title = "Wie klingen die Stimmen von Sängern?",
                                  admin_password = "vocaloid",
                                  randomise_stimuli_at_runtime = TRUE,
                                  researcher_email = NULL,
@@ -25,8 +25,56 @@ voice_rating_battery <- function(title = "Voiceratingbattery",
                                  logo = NULL,
                                  debug = FALSE) {
   info_p1 <-
-    shiny::HTML(
-      "Voice rating info"
+    # shiny::HTML(
+    #   "Voice rating info"
+    # )
+    shiny::div(
+      shiny::p(
+        "Vielen Dank, dass Sie an unserer Studie teilnehmen wollen!",
+        "Wir interessieren uns dafür, wie für Sie die Stimmen verschiedener Sänger klingen: künstlich oder menschlich?",
+        "Dabei gibt es kein richtig oder falsch.",
+        "Sie werden 60 kurze Ausschnitte hören bewerten.",
+        "Der Fragebogen startet mit Fragen zu Ihrer Person.",
+        "Er dauert insgesamt ca. 45 min [check!!!]."
+      ),
+      shiny::p(
+        "Ihre Teilnahme ist freiwillig.",
+        shiny::tags$b("Voraussetzung zur Teilnahme ist, dass Sie mindestens 18 Jahre alt sind, Sie keine fundierten Japanischen Sprachkenntnisse haben, und Deutsch Ihre Muttersprache ist."),
+        "Sie können Ihre Teilnahme jederzeit ohne Angabe von Gründen beenden, aber es würde uns helfen, wenn Sie bis zum Ende alle Songs bewerten könnten.",
+        "Bei vorzeitigem Abbruch werden Ihre Daten gelöscht."
+      ),
+      shiny::p(
+        "Alle Informationen, die wir durch die Befragung von Ihnen erhalten, werden vertraulich behandelt und sind anonymisiert, d.h. können in keiner Weise zu Ihrer Person zurückverfolgt werden.",
+        "Die erhobenen, anonymisierten Daten dienen der Grundlagenforschung und werden sowohl in Fachzeitschriften oder Konferenzpräsentationen veröffentlich, als auch in öffentlich zugänglichen Datenbanken,",
+        shiny::HTML("um <q>open science</q>, d.h. die Nachvollziehbarkeit wissenschaftlicher Forschung, zu unterstützen.")
+      ),
+      shiny::p(
+        "Bei Fragen oder Anregungen kontaktieren Sie bitte die Leiterin der Studie Dr. Elke Lange, ",
+        shiny::HTML("<a href=elke.lange@ae.mpg.de>elke.lange@ae.mpg.de</a>,"),
+        "Senior Researcher, Max-Planck-Institut für Empirische Ästhetik, Frankfurt am Main."
+      ),
+      shiny::p(
+        "Durch das Klicken auf die Schaltfläche",
+        shiny::HTML("<q>Ich stimme den Teilnahmebedingungen zu und möchte an der Studie teilnehmen.</q>"),
+        "geben Sie Ihr Einverständnis zu den oben genannten Bedingungen."
+      )
+    )
+  info_pre_stimuli <-
+    shiny::div(
+      shiny::p(
+        "Es folgen nun 60 kurze Ausschnitte mit Pop-songs.",
+        "Manche der Stimmen könnten künstlicher und andere menschlicher klingen.",
+        "Es gibt keine richtigen oder falschen Antworten. Uns interessiert Ihre Meinung.",
+        "Bitte überlegen Sie, ob die jeweilige Gesangsstimme für Sie eher künstlich oder eher menschlich klingt.",
+        "Sie können Ihr Urteil über die Skala eingeben und dazu den runden Knopf nach links oder rechts verschieben,",
+        shiny::HTML("z.&nbsp;B. in Richtung <q>künstlich</q> oder in Richtung <q>menschlich</q>.")
+      ),
+      shiny::p(
+        "Damit es ein wenig abwechslungsreicher ist, haben wir 30 songs gewählt,",
+        "bei denen die Sänger auf Japanisch singen und 30,",
+        shiny::HTML("bei denen die Sänger Silben wie <q>lalala</q> verwenden.")
+      ),
+      shiny::p("Nun geht es los!")
     )
   # info_plast <-
   #   shiny::HTML(
@@ -37,7 +85,7 @@ voice_rating_battery <- function(title = "Voiceratingbattery",
       psychTestR::new_timeline(
         psychTestR::one_button_page(
           body = info_p1,
-          button_text = psychTestR::i18n("CONTINUE")
+          button_text = psychTestR::i18n("CONSENT_BUTTON"),
         ),
         dict = dict
       ),
@@ -62,6 +110,13 @@ voice_rating_battery <- function(title = "Voiceratingbattery",
       ),
       psychTestR::elt_save_results_to_disk(complete = FALSE),
       psychTestR::end_module(),
+      psychTestR::new_timeline(
+        psychTestR::one_button_page(
+          body = info_pre_stimuli,
+          button_text = psychTestR::i18n("CONTINUE")
+        ),
+        dict = dict
+      ),
       psychTestR::new_timeline(
       audio_vas_page_battery(
         battery_label = "voice_rating",
