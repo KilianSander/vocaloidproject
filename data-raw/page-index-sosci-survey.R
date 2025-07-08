9ff19a4e31c890f911d0f5f7d70bd84fadbcdb94
Kilian <kilian.sander@aol.com>
2025-07-08 12:17:56 +0200

    prepare SoSci Survey page indexes for 60 stimuli

--- data-raw/page-index-sosci-survey.R
+++ data-raw/page-index-sosci-survey.R
@@ -1,6 +1,6 @@
 ## get php array index for 45 stimulus pages and 6 break pages in soscisurvey
 library(tidyverse)
-df <-
+df45 <-
   data.frame(
     index = 0:50,
     page = c(
@@ -18,5 +18,27 @@ df <-
       str_c("stimulus_", 39:45)
     )
   )
-df %>%
+df45 %>%
+  filter(str_detect(page, "block"))
+
+## get php array index for 60 stimulus pages and 6 break pages in soscisurvey
+df60 <-
+  data.frame(
+    index = 0:65,
+    page = c(
+      "block1",
+      str_c("stimulus_", 1:10),
+      "block2",
+      str_c("stimulus_", 11:20),
+      "block3",
+      str_c("stimulus_", 21:30),
+      "block4",
+      str_c("stimulus_", 31:40),
+      "block5",
+      str_c("stimulus_", 41:50),
+      "block6",
+      str_c("stimulus_", 51:60)
+    )
+  )
+df60 %>%
   filter(str_detect(page, "block"))
