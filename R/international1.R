cd504720b0d514f405b3d7a9e1a4814685dda4a3
Kilian <kilian.sander@aol.com>
2025-08-13 19:13:04 +0200

    outline function for international experiment 1

---
+++ R/international1.R
@@ -0,0 +1,127 @@
+#' Battery for International Experiment 1
+#'
+#'
+#' @param gms_subscales (character vector) passed to the `subscales` argument of
+#' [psyquest::GMS()].
+#'
+#' @param technical_error_back_link (character scalar) link for returning to
+#' Prolific when URL parameter `uses` or `udes` is missing.
+#' It has to contain the placeholder `%s` for `p_id` to redirect
+#' participants correctly.
+#'
+#' @param debug (logical scalar) `r lifecycle::badge("experimental")` whether
+#' or not to display debug information.
+#'
+#' @inheritParams emotional_baseline_page
+#' @inheritParams psyquest::DEG
+#' @inheritParams psychTestR::test_options
+#'
+international1 <- function(title = "",
+                           admin_password = "vocaloid",
+                           researcher_email = "kilian.vogt@hmtm-hannover.de",
+                           dict = vocaloidproject::vocaloidproject_dict,
+                           allow_any_p_id_url = TRUE,
+                           force_p_id_from_url = FALSE,
+                           languages = c("de_f", "ja"),
+                           year_range = c(1925, 2007),
+                           gms_subscales = c("General"),
+                           logo = NULL,
+                           technical_error_back_link = "",
+                           debug = FALSE){
+  #
+  elts <-
+    psychTestR::join(
+      session_design_url_welcome_page(
+        dict = dict,
+        debug = debug
+      ),
+      psychTestR::conditional(
+        test = function(state, ...) {
+          any(
+            purrr::map_lgl(
+              c("uses", "udes"),
+              function(x) {
+                is.null(psychTestR::get_global(key = x, state = state))
+              }
+            )
+          )
+        },
+        logic = last_page_redirect(
+          redirect_heading = "thanks",
+          back_link_key = "return_to_prolific",
+          back_link = technical_error_back_link
+        )
+      ),
+      psychTestR::conditional(
+        test = function(state, ...) {
+          psychTestR::get_global(key = "uses", state = state) == 1
+        },
+        logic = international1_session_1(
+          dict = dict,
+          year_range = year_range
+        )
+      ),
+      psychTestR::conditional(
+        test = function(state, ...) {
+          psychTestR::get_global(key = "uses", state = state) == 2
+        },
+        logic = psyquest::GMS(
+          dict = dict,
+          subscales = gms_subscales
+        )
+      ),
+      emotional_baseline_page(
+        dict = dict,
+        vertical = FALSE # DOES NOT LOOK PRETTY ON MOBILE DEVICES!!
+      ),
+      # emotional_baseline_single_pages(
+      #   dict = dict
+      # ),
+      psychTestR::elt_save_results_to_disk(complete = TRUE),
+      # preliminary
+      psychTestR::final_page("Finale")
+    )
+  #
+  psychTestR::make_test(
+    elts = elts,
+    opt = psychTestR::test_options(
+      title = title,
+      admin_password = admin_password,
+      researcher_email = researcher_email,
+      demo = FALSE,
+      languages = languages,
+      allow_any_p_id_url = allow_any_p_id_url,
+      force_p_id_from_url = force_p_id_from_url,
+      logo = logo,
+      logo_width = "300px",
+      logo_height = "auto"
+    )
+  )
+}
+
+international1_session_1 <- function(dict = vocaloidproject::vocaloidproject_dict,
+                                     year_range = c(1925, 2007)) {
+  psychTestR::join(
+    psyquest::DEG(
+      subscales = c("Age", "Gender"),
+      # show_month = FALSE,
+      year_range = year_range,
+      dict = dict
+    ),
+    first_language_page(
+      dict = dict
+    ),
+    education_page(
+      dict = dict
+    ),
+    student_page(
+      dict = dict
+    ),
+    field_page(
+      dict = dict
+    ),
+    japanese_skills_page(
+      dict = dict
+    )
+  )
+}
