862cff0c4840649664b17fd2acc99475444d0b63
Kilian <kilian.sander@aol.com>
2025-08-13 17:50:44 +0200

    url parameter welcome page

--- R/url-params.R
+++ R/url-params.R
@@ -1,5 +1,49 @@
-#' Functions to handle URL parameters
+#' Welcome page with URL parameter retrieval
 #'
+#' This functions creates a welcome page.
+#' It checks whether the URL parameters `uses` (for session) and `udes`
+#' (for design) are present.
+#' After completing the page, both URL parameters are set as global variables
+#' (with [psychTestR::set_global()] in the `on_complete` argument of the page).
 #'
-
+#' @inheritParams emotional_baseline_page
+#'
+#' @param debug (scalar logical) whether or not to display the URL parameter
+#' `udes` (i.e., the design) on the page.
+session_design_url_welcome_page <- function(dict = vocaloidproject::vocaloidproject_dict,
+                                            default_lang = "de_f",
+                                            debug = FALSE) {
+  psychTestR::new_timeline(
+    psychTestR::reactive_page(
+      function(state, ...) {
+        url_params <- psychTestR::get_url_params(state)
+        psychTestR::one_button_page(
+          if (any(purrr::map_lgl(c("uses", "udes"), function(x) is.null(url_params[[x]])))) {
+            shiny::p("Something went wrong!")
+          } else {
+            shiny::div(
+              shiny::h3(
+                psychTestR::i18n("WELCOME")
+              ),
+              if (debug) {
+                shiny::p(sprintf("Design %s", url_params$udes))
+              },
+              shiny::p(
+                psychTestR::i18n(paste0("SESSION", url_params$uses, "OF2"))
+              )
+            )
+          },
+          button_text = psychTestR::i18n("CONTINUE"),
+          on_complete = function(state, ...) {
+            psychTestR::set_global(key = "uses", value = url_params$uses, state = state)
+            psychTestR::set_global(key = "udes", value = url_params$udes, state = state)
+            # purrr::walk(c("uses", "udes"), function(x) {psychTestR::set_global(key = x, value = url_params$x, state = state)})
+          }
+        )
+      }
+    ),
+    dict = dict,
+    default_lang = default_lang
+  )
+}
 
--- trying-url-params.R
+++ trying-url-params.R
@@ -1,9 +1,9 @@
 library(psychTestR)
 
 elts <-
-  join(
+  psychTestR::join(
     # one_button_page("Hello"),
-    reactive_page(
+    psychTestR::reactive_page(
       function(state, ...) {
         url_params <- psychTestR::get_url_params(state)
         # set_global(
@@ -16,7 +16,7 @@ elts <-
         #   state = state,
         #   value = url_params$udes
         # )
-        one_button_page(
+        psychTestR::one_button_page(
           if (is.null(url_params$uses) | is.null(url_params$udes)) {
             shiny::p("At least 1 URL parameter is missing!")
           } else {
@@ -34,29 +34,29 @@ elts <-
             )
           },
           on_complete = function(state, ...) {
-            set_global(key = "uses", value = url_params$uses, state = state)
-            set_global(key = "udes", value = url_params$udes, state = state)
+            psychTestR::set_global(key = "uses", value = url_params$uses, state = state)
+            psychTestR::set_global(key = "udes", value = url_params$udes, state = state)
           }
         )
       }
     ),
-    reactive_page(
+    psychTestR::reactive_page(
       function(state, ...) {
-        one_button_page(
+        psychTestR::one_button_page(
           shiny::p(
             sprintf(
               "https://soscisurvey.de/vocaloidproject/?uses=%s&udes=%s",
-              get_global(key = "uses", state = state),
-              get_global(key = "udes", state = state)
+              psychTestR::get_global(key = "uses", state = state),
+              psychTestR::get_global(key = "udes", state = state)
             )
           )
         )
       }
     ),
-    final_page("Finish!")
+    psychTestR::final_page("Finish!")
   )
 
-make_test(
+psychTestR::make_test(
   elts = elts,
-  opt = test_options(title = "URL params", admin_password = "1234")
+  opt = psychTestR::test_options(title = "URL params", admin_password = "1234",languages = c("de_f", "de", "en", "ja"))
 )
