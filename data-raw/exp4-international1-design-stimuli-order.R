bc323dfc2b6937127c3791cfa322c1c5f3352514
Kilian <kilian.sander@aol.com>
2025-11-08 19:58:59 +0100

    prepare stimulus presentation order designs for sosci survey

---
+++ data-raw/exp4-international1-design-stimuli-order.R
@@ -0,0 +1,67 @@
+# Design Stimuli Presentation Order for Experiment 4 (International 1) ---------
+#
+# `data-raw/createDesign.py` creates four design files:
+# `designA.txt`, `designB.txt`, `designC.txt`, and `designD.txt`.
+# For details see Elke's documentation in `data-raw/createDesign.py`.
+#
+## Read design files -----------------------------------------------------------
+require(dplyr)
+read_design <- function(file) {
+  read.table(
+    file = file,
+    header = FALSE,
+    sep = " ",
+    colClasses = c(NULL, rep("integer", times = 8))
+  ) %>%
+    select(
+      trial = V2,
+      session = V3,
+      block = V4,
+      within_block_trial = V5,
+      stimulus_excerpt_no = V6,
+      condition = V7,
+      coding_type_of_serial_order_of_conditions = V8,
+      counter_for_stimuli = V9
+    ) %>%
+# Append a column to design data frae to display the combination of condition
+# and stimulus excerpt number.
+    mutate(
+      stimulus_id = sprintf(
+        "%s%02i", condition, stimulus_excerpt_no
+      )
+    )
+}
+design_a <- read_design("data-raw/designA.txt")
+design_b <- read_design("data-raw/designB.txt")
+design_c <- read_design("data-raw/designC.txt")
+design_d <- read_design("data-raw/designD.txt")
+## create vectors for each session and design ----------------------------------
+for (l in letters[1:4]) {
+  for (s in 1:2) {
+    assign(
+      x = paste0(l, s),
+      value = get(paste0("design_", l)) %>%
+        filter(session == s) %>%
+        pull("stimulus_id"),
+      envir = .GlobalEnv
+    )
+  }
+}
+## Output for SoSci Survey -----------------------------------------------------
+# Generate a php array from the vectors
+output_for_sosci <-
+  "data-raw/exp4-international1-design-stimuli-order-php-arrays.txt"
+file.create(output_for_sosci)
+for (l in letters[1:4]) {
+  for (s in 1:2) {
+    paste0(
+      "$", l, s, " = array(",
+      paste0("'", get(paste0(l, s)), "'", collapse = ", "),
+      ");\n"
+    ) %>%
+      cat(
+        file = output_for_sosci,
+        append = TRUE
+      )
+  }
+}
