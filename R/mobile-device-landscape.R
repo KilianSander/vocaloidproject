90ac3cbb273b8b54857d4375e4de55b46acbc1d9
Kilian <kilian.sander@aol.com>
2025-11-09 18:40:08 +0100

    prepare page to prompt participant to hold their mobile device in landscape mode

---
+++ R/mobile-device-landscape.R
@@ -0,0 +1,7 @@
+#' Mobile devices landscape
+#'
+#' Please use your mobile device in landscape mode.
+#' https://s3.eu-west-1.amazonaws.com/media.dots.org/img/mobile-device-landscape-small.png
+use_landscape_page <- function() {
+  #
+}
