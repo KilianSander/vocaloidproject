8ac99cd46f7070c11c64c9a0c37b366ab0fb3590
Kilian Sander <kilian.sander@aol.com>
2025-11-17 11:55:17 +0100

    :white_check_mark: add imports to appease `devtools::check()`

--- R/vocaloidproject-package.R
+++ R/vocaloidproject-package.R
@@ -5,6 +5,8 @@
 #' @importFrom lifecycle deprecated
 #' @importFrom magrittr %>%
 #' @importFrom methods is
+#' @importFrom stats setNames
+#' @importFrom utils read.delim
 #' @importFrom utils write.csv
 ## usethis namespace: end
 NULL
