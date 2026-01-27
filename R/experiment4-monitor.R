d4f52d16b48a5f0fb4f5033d6ec73d461b0e963a
Kilian <kilian.sander@aol.com>
2026-01-27 11:54:08 +0100

    :construction: add arguments to `experiment4_monitor()`

--- R/experiment4-monitor.R
+++ R/experiment4-monitor.R
@@ -6,17 +6,37 @@
 #' The function assumed that it is running on the same shiny server as
 #' [experiment4()].
 #'
-#' @param battery_folder_name (character) This is the name of the folder(s)
-#' on your shiny server where the `app.R` containing `experiment4(...)`
-#' lives.
+#' @param battery_folder_1 (character scalar) This is the name of the folder
+#' on your shiny server where the `app.R` containing
+#' `experiment4(session_number = 1, ...)`
+#' lives (i.e. app for Session 1).
+#'
+#' @param battery_folder_2 (character scalar) This is the name of the folder
+#' on your shiny server where the `app.R` containing
+#' `experiment4(session_number = 2, ...)`
+#' lives (i.e. app for Session 2).
+#'
+#' @param battery_folder_3 (character scalar) This is the name of the folder
+#' on your shiny server where the `app.R` containing
+#' `experiment4(session_number = 3, ...)`
+#' lives (i.e. app for Session 3).
+#'
+#' @param sosci_data (`NULL` or character scalar) if non-`NULL`, a URL to
+#' load data from (provided by SoSci Survey).
 #'
 #' @param data_pw (character scalar) `r lifecycle::badge("experimental")`
 #' set a password for data access.
 #' Caution: This is a very simple implementation.
 #'
-experiment4_monitor <- function(battery_folder_name = "",
+experiment4_monitor <- function(battery_folder_1 = "",
+                                battery_folder_2 = "",
+                                battery_folder_3 = "",
+                                sosci_data = NULL,
                                 data_pw = "supersecretpassword") {
-  stopifnot(is.character(battery_folder_name),
+  stopifnot(is.scalar.character(battery_folder_1),
+            is.scalar.character(battery_folder_2),
+            is.scalar.character(battery_folder_3),
+            is.scalar.character(sosci_data) | is.null(sosci_data),
             is.scalar.character(data_pw))
   ui <- bslib::page_sidebar(
     title = "Vocaloid Project Experiment 4 Data Monitor",
@@ -27,7 +47,7 @@ experiment4_monitor <- function(battery_folder_name = "",
       ),
       shiny::actionButton(
         "get_data",
-        "Get Data!"
+        "Get Data From Server!"
       )
     ),
     bslib::layout_columns(),
